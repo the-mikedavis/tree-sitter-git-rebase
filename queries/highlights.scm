dd50001eadb3ad67972adf250e98f71c567a4d21
Michael Davis <mcarsondavis@gmail.com>
2021-12-20 09:16:56 -0600

    draft highlight queries

---
+++ queries/highlights.scm
@@ -0,0 +1,15 @@
+(operation operator: ["p" "pick" "r" "reword" "e" "edit" "s" "squash" "m" "merge" "d" "drop" "b" "break" "x" "exec"] @function.builtin)
+(operation operator: ["l" "label" "t" "reset"] @function)
+(operation operator: ["f" "fixup"] @attribute)
+
+(option) @operator
+
+(message) @string
+
+(label) @constant
+
+(commit) @constant.builtin
+
+"#" @punctation.delimiter
+
+(comment) @comment
