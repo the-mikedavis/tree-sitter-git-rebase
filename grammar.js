8f16c6a5802d5b0ce96bae877a44ea1aadd3eb9f
Michael Davis <mcarsondavis@gmail.com>
2021-12-19 22:51:38 -0600

    implement everything up to merge

--- grammar.js
+++ grammar.js
@@ -1,63 +1,83 @@
+// helper constants
 const WHITE_SPACE = choice(" ", "\t", "\v", "\f");
 const NEWLINE = /\r?\n/;
+const ANYTHING = /.*/;
 
-const PICK = choice("pick", "p");
-const REWORD = choice("reword", "r");
-const EDIT = choice("edit", "e");
-const SQUASH = choice("squash", "s");
-const FIXUP = choice("fixup", "f");
-const EXEC = choice("exec", "x");
-const BREAK = choice("break", "b");
-const DROP = choice("drop", "d");
-const LABEL = choice("label", "l");
-const RESET = choice("reset", "t");
-const MERGE = choice("merge", "m");
+// operators
+const PICK = field("operator", choice("pick", "p"));
+const REWORD = field("operator", choice("reword", "r"));
+const EDIT = field("operator", choice("edit", "e"));
+const SQUASH = field("operator", choice("squash", "s"));
+const FIXUP = field("operator", choice("fixup", "f"));
+const EXEC = field("operator", choice("exec", "x"));
+const BREAK = field("operator", choice("break", "b"));
+const DROP = field("operator", choice("drop", "d"));
+const LABEL = field("operator", choice("label", "l"));
+const RESET = field("operator", choice("reset", "t"));
+const MERGE = field("operator", choice("merge", "m"));
 
 module.exports = grammar({
   // hyphens are not allowed in grammar names
   name: "gitrebasetodo",
 
+  // this grammar is very rigid about whitespace, so I'm taking
+  // full control of whitespace
   extras: ($) => [],
 
   rules: {
-    source: ($) => repeat($._command),
+    source: ($) => repeat($._line),
 
-    _command: ($) =>
-      choice(seq($.operation, NEWLINE), seq(optional(WHITE_SPACE), NEWLINE)),
+    _line: ($) =>
+      choice(
+        seq($.operation, NEWLINE),
+        seq(optional(repeat(WHITE_SPACE)), NEWLINE),
+        seq(optional(repeat(WHITE_SPACE)), $.comment, NEWLINE)
+      ),
 
     operation: ($) =>
       choice(
         // $._merge,
-        // $._break,
-        // $._commit_operation_with_option,
-        $._commit_operation_without_option
-        // $._label_operation,
+        $._fixup,
+        $._commit_operation_without_option,
+        $._label_operation,
+        $._exec,
+        $._break
       ),
 
-    // _break: $ => BREAK,
+    _fixup: ($) =>
+      seq(
+        FIXUP,
+        WHITE_SPACE,
+        optional(seq($.option, WHITE_SPACE)),
+        $.commit,
+        optional(seq(WHITE_SPACE, $.message))
+      ),
 
     _commit_operation_without_option: ($) =>
       seq(
-        choice(PICK, REWORD, EDIT, SQUASH),
+        choice(PICK, REWORD, EDIT, SQUASH, DROP),
         WHITE_SPACE,
         $.commit,
-        WHITE_SPACE,
-        optional(alias($._implicit_comment, $.comment))
+        optional(seq(WHITE_SPACE, $.message))
       ),
 
-    // _label_operation: $ => seq(
-    //   choice(LABEL, RESET),
-    //   $.label,
-    // ),
+    _label_operation: ($) => seq(choice(LABEL, RESET), WHITE_SPACE, $.label),
 
-    // TODO: may need to use the external scanner for this
-    label: ($) => /\w\d-_/,
+    _exec: ($) => seq(EXEC, WHITE_SPACE, $.command),
+
+    _break: ($) => BREAK,
+
+    // maybe this should be /-[a-zA-Z]/?
+    option: ($) => choice("-c", "-C"),
+
+    label: ($) => /[^\s]+/,
 
     commit: ($) => /[a-f0-9]{7,40}/,
 
-    _explicit_comment: ($) => token(seq("#", /.*/)),
-    _implicit_comment: ($) => token(repeat(choice(WHITE_SPACE, /.*/))),
+    message: ($) => ANYTHING,
 
-    comment: ($) => choice($._explicit_comment, $._implicit_comment),
+    command: ($) => ANYTHING,
+
+    comment: ($) => token(seq("#", ANYTHING)),
   },
 });
