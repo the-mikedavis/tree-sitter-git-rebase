0eab47b583e7c8b7deac8146ccd0f8e689e6d9d7
Michael Davis <mcarsondavis@gmail.com>
2021-12-19 20:23:43 -0600

    add comment about tree-sitter package

--- flake.nix
+++ flake.nix
@@ -10,6 +10,10 @@
         {
           devShell = pkgs.mkShell {
             buildInputs = with pkgs; [
+              # I have a custom fork I'm using but others may want 'tree-sitter'
+              # or you can use 'npx tree-sitter <command>'
+              # tree-sitter
+              #
               # needed to build the grammar with 'tree-sitter generate'
               nodejs
               # compiles parser.c and other relavent c code in the grammar
